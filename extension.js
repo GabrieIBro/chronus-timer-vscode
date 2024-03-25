66f4c1177484f0aefd409b618404a3628288ad36
GabrieIBro <gabriel.fsans@hotmail.com>
2024-03-25 09:23:21 -0300

    Store data in globalState

--- extension.js
+++ extension.js
@@ -1,33 +1,55 @@
 const vscode = require('vscode');
 
+
 /**
  * @param {vscode.ExtensionContext} context
  */	
 let myStatusBarItem;
+let timerIsRunning = false;
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let time;			
+let date = new Date;
 
 function activate(context) {
+
     myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1);
-    context.subscriptions.push(myStatusBarItem);
-
-	let timerIsRunning = false;
-
-	let seconds = 0;
-	let minutes = 0;
-	let hours = 0;
-	let time;			
 
 	let configuration = vscode.workspace.getConfiguration('code-timer');
 	configuration.update("pauseTimerWhenUnfocused", true, vscode.ConfigurationTarget.Global);
 
 	let pauseWhenUnfocused = configuration.get("pauseTimerWhenUnfocused");
 
+	let currentDate = {"day":date.getDate(), "month": date.getMonth(), "year":date.getFullYear()}
+	let dateTemplate = `${currentDate.day}/${currentDate.month}/${currentDate.year}`;
+
+	// Verify if session count is already stored. If already stored, increment by 1 on every activate.
+	let currentDateObj = context.globalState.get(dateTemplate) || {};
+	if(!currentDateObj.sessions) {currentDateObj.sessions = 0}; 
+	currentDateObj.sessions++;
+
+	let session = currentDateObj.sessions;
+	console.log(session)
+
+	context.globalState.update(dateTemplate, {...currentDateObj});
 
 	let startTimer = vscode.commands.registerCommand('code-timer.startTimer', function () {
 		timerIsRunning = true;
 
-
 		const timer = setInterval(() => {
-			
+			// console.log(context.globalState.f);
+			let sessionList = context.globalState.get(dateTemplate) || {};
+			sessionList[`Session ${session}`] = time;
+			context.globalState.update(dateTemplate, {...sessionList})
+			console.log(context.globalState.f)
+
+			if(date.getDate() !== currentDate.day) {
+				currentDate = {"day":date.getDate(), "month": date.getMonth(), "year":date.getFullYear()}
+				seconds = minutes = hours = 0;
+			}
+
 			if(timerIsRunning) {
 				
 				if(seconds === 60){
@@ -53,32 +75,39 @@ function activate(context) {
 			}
 		}, 1000)
 
-		context.subscriptions.push(startTimer);
 	});
-
 
 	let pauseTimer = vscode.commands.registerCommand('code-timer.pauseTimer', function() {
 		timerIsRunning = false;
 		updateStatusBar(time, '$(debug-start)', "code-timer.startTimer", "start");
 
-		context.subscriptions.push(pauseTimer);
+	})
+
+	let showTimerLog = vscode.commands.registerCommand('code-timer.showTimerLog', () => {
+		let webViewElement = vscode.window.createWebviewPanel('string', 'Work Log', vscode.ViewColumn);
+		webViewElement.webview.html = `<p>${time}</p>`;
+		webViewElement.visible = true;
+		
 	})
 
 	vscode.commands.executeCommand("code-timer.startTimer");
 
 	if(pauseWhenUnfocused) {
 		vscode.window.onDidChangeWindowState(event => {
-			console.log(event);
 			if(!event.focused) {
 				vscode.commands.executeCommand("code-timer.pauseTimer")
 			}
-			
+
 			if(event.focused && !timerIsRunning) {
 				vscode.commands.executeCommand("code-timer.startTimer");
-				console.log("StartTimer executed")
 			}
 		})
 	}
+
+	context.subscriptions.push(startTimer);
+	context.subscriptions.push(pauseTimer);
+	context.subscriptions.push(myStatusBarItem);
+	context.subscriptions.push(showTimerLog);
 }
 
 function updateStatusBar(time, icon='$(debug-pause)', buttonCommand, tooltipText) {
@@ -88,6 +117,7 @@ function updateStatusBar(time, icon='$(debug-pause)', buttonCommand, tooltipText
 	myStatusBarItem.tooltip = `Click to ${tooltipText}`;
 	myStatusBarItem.accessibilyInfomation = time;
 }
+
 
 
 function deactivate() {}
