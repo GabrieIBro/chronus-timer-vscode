9e8ed74d6194f819d032b2ad0d2054a9719b805f
GabrieIBro <gabriel.fsans@hotmail.com>
2024-04-08 17:13:48 -0300

    Add reset timer logs function

--- extension.js
+++ extension.js
@@ -1,5 +1,4 @@
 const vscode = require('vscode');
-const fs = require('fs')
 const { newDB, getDB, updateDB } = require('./database')
 
 /**
@@ -26,51 +25,72 @@ function activate(context) {
 
 	let currentDate = {"day":date.getDate(), "month": date.getMonth() + 1, "year":date.getFullYear()}
 	let dateTemplate = `${(currentDate.day < 10) ? '0' : ''}${currentDate.day}/${(currentDate.month < 10) ? '0' : ''}${currentDate.month}/${currentDate.year}`;
+	let session;
 
-	let currentDateArray = context.globalState.get(dateTemplate) || [];
-	currentDateArray.push('Placeholder');
-	context.globalState.update(dateTemplate, [...currentDateArray]);
+	getDB()
+	.then(res => {
+		if(res[dateTemplate] === undefined) {
+			res[dateTemplate] = [];
+		}
 
-	let session = currentDateArray.length;
+		res[dateTemplate].push('00h 00m 00s');
+		session = res[dateTemplate].length;
+		updateDB(res)
+	})
+	.catch(err => {
+		console.log(err);
+	})
+
 
 	//Add new instance and check if an instance stopped running
-	context.globalState.update('31/3/2024', undefined);
 
-	let instances = context.globalState.get('instances') || {};
+	// context.globalState.update('31/3/2024', undefined);
+
+	// let instances = context.globalState.get('instances') || {};
 	
-	let instanceID = String(Math.random().toFixed(10)).slice(2);
+	// let instanceID = String(Math.random().toFixed(10)).slice(2);
 
-	let lastInstance = context.globalState.get('lastInstance') || 0;
+	// let lastInstance = context.globalState.get('lastInstance') || 0;
 
-	if(lastInstance - Date.now() < -5000) {
-		console.log("Clean Instances")
-		// modifyDB(false, true);
-	}
+	// if(lastInstance - Date.now() < -5000) {
+	// 	console.log("Clean Instances")
+	// 	// modifyDB(false, true);
+	// }
 
-	setInterval(()=>{
-		instances[instanceID] = Date.now();
-		context.globalState.update('instances', instances);
-		context.globalState.update('lastInstance', Date.now());
-	}, 1000)
+	// setInterval(()=>{
+	// 	instances[instanceID] = Date.now();
+	// 	context.globalState.update('instances', instances);
+	// 	context.globalState.update('lastInstance', Date.now());
+	// }, 1000)
 	
-	let instancesLength = Object.keys(instances);
+	// let instancesLength = Object.keys(instances);
 
-	if(instancesLength === 1) {
-		isMain = true;
-	}
+	// if(instancesLength === 1) {
+	// 	isMain = true;
+	// }
 
 
 
 	// Functions
+	
 	let startTimer = vscode.commands.registerCommand('chronus.startTimer', function () {
 		timerIsRunning = true;
 
 		timer = setInterval(() => {
-			// Update time on globalState
-			let sessionList = context.globalState.get(dateTemplate) || [];
-			sessionList[session - 1] = time;
+			// Update time on DB
+			getDB()
+			.then(res => {
+				// if(!res[dateTemplate]) {
+				// 	res[dateTemplate] =('00h 00m 00s');
+				// }
 
-			context.globalState.update(dateTemplate, [...sessionList])
+				res[dateTemplate][session - 1] = time;
+				updateDB(res);
+			})
+			.catch(err => {
+				console.log(err);
+			})
+
 
 			// Get current date
 			if(date.getDate() !== currentDate.day) {
@@ -112,59 +132,91 @@ function activate(context) {
 	let showTimerLog = vscode.commands.registerCommand('chronus.showTimerLog', function() {
 		let webViewElement = vscode.window.createWebviewPanel('string', 'Work Log', vscode.ViewColumn.One);
 
-		let keys = context.globalState.keys();
-		let htmlContent = "";
+		getDB()
+		.then(res => {
+			let keys = Object.keys(res);
 
-		keys.forEach(key => {
+			let htmlContent = "";
 
-			if(key.includes('/')) {
-				htmlContent += `
-				<details>
-					<summary>${[key]}</summary>
-								`;
+			keys.forEach(key => {
 
-				context.globalState.get(key).forEach((el, index) => {
-				htmlContent += `
-					<ul><li>Session ${[index + 1]}: ${[el]}</li></ul>
-								`;
-				})
-				htmlContent += `
-				<style>
-					@import url('https://fonts.googleapis.com/css2?family=Signika:wght@300..700&display=swap');
+				if(key.includes('/')) {
+					htmlContent += `
+					<details>
+						<summary>${[key]}</summary>
+									`;
 
-					details {
-						font-family:Signika, Garamond;
-					}
+					res[key].forEach((el, index) => {
+						htmlContent += `
+							<ul><li>Session ${[index + 1]}: ${[el]}</li></ul>
+										`;
+					})
+					htmlContent += `
+					<style>
+						@import url('https://fonts.googleapis.com/css2?family=Signika:wght@300..700&display=swap');
 
-					li {
-						font-size:18px;
-						font-weight:400;
-						color:white;
-					}
+						details {
+							font-family:Signika, Garamond;
+						}
 
-					summary {
-						font-size:28px; 
-						font-weight:600;
-						user-select: none;
-						transition: ease-out 150ms;
-						color:white;
-					}
+						li {
+							font-size:18px;
+							font-weight:400;
+							color:white;
+						}
 
-					summary:hover {
-						cursor:pointer;
-						color: #bababa;
+						summary {
+							font-size:28px; 
+							font-weight:600;
+							user-select: none;
+							transition: ease-out 150ms;
+							color:white;
+						}
 
-					}
+						summary:hover {
+							cursor:pointer;
+							color: #bababa;
 
-				</style>
-				</details>`;
-			}
+						}
+
+					</style>
+					</details>`;
+				}
+				
+			});
+
+			webViewElement.webview.html = htmlContent;
+			webViewElement.visible = true;
 			
-		});
 
-		webViewElement.webview.html = htmlContent;
-		webViewElement.visible = true;
+		})
+	})
+
+	let resetLogs = vscode.commands.registerCommand('chronus.resetLogs', async function() {
+		time = '00h 00m 00s';
+		hours = minutes = seconds = 0;
+		vscode.commands.executeCommand('chronus.pauseTimer');
+
+		let reload = await vscode.window.showWarningMessage(
+			'This function will reload the workspace. Do you want to proceed?',
+			{ modal: true },
+			'Reload'
+		);
 		
+		if(reload) {
+					getDB()
+		.then(res => {
+			let keys = Object.keys(res);
+
+			keys.forEach(key => {
+				if(key.includes('/')) {
+					delete res[key];
+				}
+			})
+			updateDB(res);
+			vscode.commands.executeCommand('workbench.action.reloadWindow');
+		})
+		}
 	})
 
 	let logGlobalState = vscode.commands.registerCommand('chronus.logGlobalState', () => {
@@ -175,7 +227,8 @@ function activate(context) {
 	vscode.commands.executeCommand("chronus.startTimer");
 	vscode.workspace.onDidChangeConfiguration(() => {
 		configuration = vscode.workspace.getConfiguration('chronus');
-		pauseWhenUnfocused = configuration.get("pauseTimerWhenUnfocused");				
+		pauseWhenUnfocused = configuration.get("pauseTimerWhenUnfocused");	
+		console.log('pauseWhenUnfocused: ' + pauseWhenUnfocused);			
 	});
 
 	let listener = vscode.window.onDidChangeWindowState((event) => {
@@ -197,6 +250,7 @@ function activate(context) {
 	context.subscriptions.push(myStatusBarItem);
 	context.subscriptions.push(showTimerLog);
 	context.subscriptions.push(logGlobalState);
+	context.subscriptions.push(resetLogs);
 
 }
 
@@ -207,7 +261,6 @@ function updateStatusBar(time, icon='$(debug-pause)', buttonCommand, tooltipText
 	myStatusBarItem.tooltip = `Click to ${tooltipText}`;
 	myStatusBarItem.accessibilyInfomation = time;
 }
-
 
 function deactivate() {}
 
